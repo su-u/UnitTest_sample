1a7bf7625b37980843a478fe716fb8dc7de272ea
su- <aaaiiiuuu35@gmail.com>
2019-12-11 19:11:19 +0900

    wisp

---
+++ UnitTest_sample/GamePlayerTest/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using UnitTest_sample;
+
+namespace GamePlayerTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void PlayTimeTest()
+        {
+            var player = new GamePlayer("test");
+            player.AddPlayTime(10);
+            Assert.AreEqual(player.TotalPlayTime, 10);
+        }
+    }
+}
--- UnitTest_sample/UnitTest_sample/Program.cs
+++ UnitTest_sample/UnitTest_sample/Program.cs
@@ -1,9 +1,29 @@
 ﻿namespace UnitTest_sample
 {
-    internal class Program
+    public class GamePlayer
     {
-        public static void Main(string[] args)
+        private string Name { get; set; } 
+        public uint TotalPlayTime { get; private set; }
+        
+
+        public GamePlayer(string name)
         {
+            this.Name = name;
+            this.TotalPlayTime = 0;
+        }
+
+        public void AddPlayTime(uint time)
+        {
+            this.TotalPlayTime += time;
+        }
+
+        public string GetPlayerStatus()
+        {
+            var status = $"name:{this.Name} playTime:${this.TotalPlayTime}";
+            return status;
         }
     }
+    
+    
+    
 }
